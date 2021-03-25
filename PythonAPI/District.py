a8abdd1ec12986b2dfa91a28873f2005ef706d9d
besarkutleshi <besarkutleshi@outlook.com>
2021-03-25 16:24:15 +0100

    Showin comulative transaction count for months

--- PythonAPI/District.py
+++ PythonAPI/District.py
@@ -5,7 +5,7 @@ class District():
     def __init__(self, data):
         self.df = data
         
-    def getTransactionCount_Month(self):
+    def get_Transaction_Count_Month(self):
         tableData = self.df.groupby(['District','Month']).size().to_frame('Count').reset_index()
         return tableData
         
@@ -17,6 +17,10 @@ class District():
         table = self.getTransactionCount_Month()
         pvtTable = pd.pivot_table(data=table,values=['Month'],index=['District','Count'])
         return pvtTable
+
+
+    def get_Transaction_Sum_Month(self):
+        return self.df.groupby(['Month']).size().to_frame('Sum').reset_index()
 
 
 
