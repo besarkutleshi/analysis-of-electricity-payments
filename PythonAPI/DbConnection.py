99af6be31058e48cb01c989dcfe20fd4e53b6cb1
Besar Kutleshi <62810874+besarkutleshi@users.noreply.github.com>
2022-08-02 15:28:44 +0200

    Update DbConnection.py

--- PythonAPI/DbConnection.py
+++ PythonAPI/DbConnection.py
@@ -2,10 +2,10 @@ import pyodbc
 class DbConnection():
     
     def get_Connection(self):
-        server = '10.150.1.70' 
-        database = 'REPORTS' 
-        username = 'besark' 
-        password = 'besark'  
+        server = '' 
+        database = '' 
+        username = '' 
+        password = ''  
         conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
         return conn
 
